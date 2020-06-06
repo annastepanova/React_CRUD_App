5c4be3a0a3ac9520fa6a416a1cd4a11d80f97170
annastepanova <annastepanova1811@gmail.com>
2020-06-05 20:17:38 -0400

    created navbar and routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,45 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import NewUser from "./components/NewUser"
+import UserInfo from "./components/UserInfo"
+import UsersList from "./components/UsersList"
+import HomePage from './components/HomePage'
 
-function App() {
+const App = () => {
   return (
-    <div>
-     
-    </div>
-  );
+    <Router>
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/" className="navbar-brand">
+            HOME
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link exact to="/users" className="nav-link">
+                ALL USERS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/users/new" className="nav-link">
+                NEW USER
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/users" component={UsersList} />
+            <Route exact path="/users/new" component={NewUser} />
+            <Route path="/users/:id" component={UserInfo} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+
+export default App
---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const HomePage = () => {
+
+  return (
+    <div>
+      Welcome to the App build with React.js, Node.js, Express, MySQL
+    </div>
+
+  )
+}
+
+export default HomePage
